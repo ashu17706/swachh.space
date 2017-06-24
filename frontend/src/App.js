2a6eb602866c471f1a7cab571686c0fd2c05192f
ashu17706 <ashu17706@gmail.com>
2017-06-24 19:00:36 +0530

    ready for first build

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Welcome to</h2>
+        <h1>swachh.space</h1>
+        <code>We are working on it.</code>
+        <br />
+        <br />
+        <code>Come back tomorrow noon.</code>
+        <br />
+        <code>AngleHack Hyderabad 2017</code>
+      </div>
+    );
+  }
+}
+
+export default App;
