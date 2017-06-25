5453db2d18fc15661130ac60a90496b92c0d8f73
Tripathi <ashish.a.tripathi@accenture.com>
2017-06-25 11:54:22 +0530

    enabled cors

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var PORT = 3000;
+var PORT = 3000
+
+var cors = require('cors')
 
 // DB and Schema settings
 require('./app/models/db');
@@ -14,6 +16,7 @@ var fetchapp = require('./app/routers/fetchapp');
 // bodyParser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/', [fetchapp, userapp]);
 
 var server = app.listen(PORT, function () {
